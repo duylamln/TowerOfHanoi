5d229c152c29c7c13dae4c2d3e7f30910dd5e76b
Lam Le <duylamln@gmail.com>
2016-04-16 11:00:40 +0700

    Change structure

---
+++ js/app.js
@@ -0,0 +1,26 @@
+﻿requirejs.config({
+    //By default load any module IDs from js/lib
+    baseUrl: 'js/lib',
+    //except, if the module ID starts with "app",
+    //load it from the js/app directory. paths
+    //config is relative to the baseUrl, and
+    //never includes a ".js" extension since
+    //the paths config could be for a directory.
+    paths: {
+        app: '../app'
+    }
+});
+
+
+// Start the main app logic.
+requirejs(['app/towerOfHanoi'], function (TowerOfHanoi) {
+    begin();
+
+    function begin() {
+        var towerOfHanoi = new TowerOfHanoi({
+            canvasElementId: "tower",
+            numberOfDisks: 8
+        });
+        towerOfHanoi.start();
+    }
+});
